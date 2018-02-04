0e8c81603a3e905cb1fd506bf1d8bc30545613ec
TheGwyd <36111148+TheGwyd@users.noreply.github.com>
2018-02-04 20:52:26 +0000

    PEP EMU partially implemented
    It doesn't work entierly. Build.bat actually works. First compile of the file.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 :start
-..gcc -C -E -nostdinc -x c-header -o RUKTS.nml RUKTS.pnml
-/nmlc.exe rukts.nml -o rukts.grf
+gcc -C -E -nostdinc -x c-header -o rukts.nml rukts.pnml
+nmlc rukts.nml -o rukts.grf
 PAUSE
 goto start
