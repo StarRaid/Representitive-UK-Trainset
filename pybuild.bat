ac5306351463d00a7e5454f308f44f95bf67d12c
StarRaid <36111067+StarRaid@users.noreply.github.com>
2018-05-23 20:20:04 +0100

    Alot of stuff that probably won't push

--- pybuild.bat
+++ pybuild.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 :start
-python nml_patcher.py -f "rukts.pnml" -o "rukts.nml" -b 1
-nmlc rukts.nml -o rukts.grf
+python nml_patcher.py -f "rukts.pnml" -o "rukts_py.nml" -b 1
+nmlc rukts.nml -o rukts_py.grf
 PAUSE
 goto start
