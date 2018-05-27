0e3aa23cee4d14b9a3f3d924d33c31a2617faa7c
TheGwyd <36111148+TheGwyd@users.noreply.github.com>
2018-05-27 10:25:51 +0100

    1025 270518

--- build.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-:start
-gcc -C -E -nostdinc -x c-header -o rukts.nml rukts.pnml
-nmlc rukts.nml -o rukts.grf
-PAUSE
-goto start
--- compile.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-:start
-../nmlc.exe rukts.nml -o rukts.grf
-PAUSE
-goto start
