e5fa72a8cc038b2653cc0d3416fecbad45e59485
TheGwyd <36111148+TheGwyd@users.noreply.github.com>
2018-05-27 09:02:42 +0100

    27/05/2018

--- pybuild.bat
+++ pybuild.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 :start
-python nml_patcher.py -f "rukts.pnml" -o "rukts.nml" -b 1
+python.exe nml_patcher.py -f "rukts.pnml" -o "rukts.nml" -b 1
 nmlc rukts.nml -o rukts.grf
 PAUSE
 goto start
