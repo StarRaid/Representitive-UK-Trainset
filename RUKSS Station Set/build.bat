de1402dffae51cdac2555bb43950794f989d9a84
TheGwyd <36111148+TheGwyd@users.noreply.github.com>
2018-09-03 21:49:31 +0200

    networkers 2

--- RUKSS Station Set/build.bat
+++ RUKSS Station Set/build.bat
@@ -1,5 +1,8 @@
+:start
 bin\m4.exe -R m4nfo_stations.m4 < src\RUKSS.nfx > src\RUKSS.nfo
 copy count.m4 + src\RUKSS.nfo test.tt
 bin\m4.exe test.tt > src\RUKSS.nfo
 nforenum.exe src\RUKSS.nfo
 grfcodec.exe -e RUKSS.grf src
+PAUSE
+goto start
