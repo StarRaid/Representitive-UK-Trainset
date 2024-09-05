5a7d04b4a53c8641e6319f9a0a15334b7795b452
StarRaid <trainlover288@gmail.com>
2024-09-05 16:00:33 +0100

    bump version to 120
    i hope the livery override is not causing a hard limitation on the 411 code.
    that would suck

--- run.bat
+++ run.bat
@@ -1,5 +1,12 @@
 @ECHO OFF
+:start
+D:
+cd "D:\OpenTTD\Representitive-UK-Trainset"
+python nml_patcher.py -f "rukts.pnml" -o "rukts.nml"
+nmlc rukts.nml -o rukts.grf -s
+copy rukts.grf "C:\Games\OpenTTD\Testbed\newgrf\rukts_dev.grf"
 C:
 cd "C:\Games\OpenTTD"
 openttd.exe -c "C:\Games\OpenTTD\Testbed\openttd.cfg"
 PAUSE
+goto start
